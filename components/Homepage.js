714910c1e620bd15a57d62d0fc84efd4de7e8cf4
Divyanshu rawat <divrawat2001@gmail.com>
2023-08-29 01:44:18 +0530

    cksljv

--- components/Homepage.js
+++ components/Homepage.js
@@ -37,28 +37,29 @@ const Homepage = () => {
 
         </div>
 
-        <div className={styles.row2}>
+        <div className={styles.twoback}>
+          <div className={styles.row2}>
 
-          <div className={styles.column3}>
-            <Image src="/greenbag.png" width={600} height={400} />
-          </div>
+            <div className={styles.column3}>
+              <Image src="/greenbag.png" width={600} height={400} />
+            </div>
 
 
-          <div className={styles.column4}>
+            <div className={styles.column4}>
 
-            <h2>Your Wellness Managed By Our CRM Technology</h2>
-            <br />
-            <br />
-            <p>The Best Wellness CRM</p>
-            <p>For Wellness Coaches</p>
-            <br />
-            <br />
+              <h2>Your Wellness Managed By Our CRM Technology</h2>
+              <br />
+              <br />
+              <p>The Best Wellness CRM</p>
+              <p>For Wellness Coaches</p>
+              <br />
+              <br />
+
+            </div>
 
           </div>
 
         </div>
-
-
       </div>
 
 
@@ -137,23 +138,115 @@ const Homepage = () => {
         <div className={styles.box2}><p>Engage With Community</p></div>
         <div className={styles.box2}><p>Get Progress Analytics</p></div>
         <div className={styles.box2}><p>Wellneess Z Team</p> </div>
-        
+
       </div>
-      
 
 
 
-<div className={styles.back3}>
 
-<div className={styles.circontainer}>
-<div className={styles.circle}>
-<div className={styles.circle2}>
-<div className={styles.circle3}></div>
-</div>
-</div>
-</div>
+      <div className={styles.back3}>
 
-</div>
+        <div className={styles.middle}>
+          <div className={styles.wellness}>Wellness Managed</div>
+          <p className={styles.wellnessp}>The Best Wellness Crm</p>
+          <p className={styles.wellnessp}>For Wellness Coaches</p>
+          <Image src="/mylogo.png" className={styles.circlelogo} height={80} width={80} />
+        </div>
+
+        <div className={styles.circlegridcontainer}>
+
+          <div className={styles.circlegriditem1}>
+            <div className={styles.iconcontainer}>
+              <Image src="/11.png" width={60} height={60} className={styles.circleimages} />
+              <p className={styles.cirpara}>Wellness</p></div>
+          </div>
+
+          <div className={styles.circlegriditem2}>
+            <div className={styles.iconcontainer}>
+              <Image src="/14.png" width={60} height={60} className={styles.circleimages} />
+              <p className={styles.cirpara}>Connection</p></div>
+          </div>
+
+
+
+          <div className={styles.circlegriditem3}>
+            <div className={styles.iconcontainer}>
+              <Image src="/12.jpg" width={60} height={60} className={styles.circleimages} />
+              <p className={styles.cirpara}>Retail</p></div>
+          </div>
+
+
+          <div className={styles.circlegriditem4}>
+            <div className={styles.iconcontainer}>
+              <Image src="/13.png" width={60} height={60} className={styles.circleimages} />
+              <p className={styles.cirpara}>Management</p></div>
+          </div>
+
+        </div>
+
+
+
+        <div className={styles.circontainer}>
+          <div className={styles.circle}>
+            <div className={styles.circle2}>
+              <div className={styles.circle3}>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+      <div className={styles.back4}>
+        <div className={styles.clientsheading}>Our Clientele</div>
+
+        <div className={styles.clientcontainer}>
+
+
+          <div className={styles.clientitem}>
+            <div className={styles.photo}><img src="/screen1.png" /></div>
+            <div className={styles.clientback}><p>Fitness Coaches</p></div>
+          </div>
+
+
+          <div className={styles.clientitem}>
+            <div className={styles.photo}><img src="/screen1.png" /></div>
+            <div className={styles.clientback}><p>Nutrition Companies</p></div>
+          </div>
+
+
+          <div className={styles.clientitem}>
+            <div className={styles.photo}><img src="/screen1.png" /></div>
+            <div className={styles.clientback}><p>Wellness Enthusiasts</p></div>
+          </div>
+
+
+        </div>
+        <div>
+        </div>
+      </div>
+
+
+
+      <div className={styles.back5}>
+        <div className={styles.givingmargin}>
+        <div className={styles.lastcontainer}>
+
+          <div className={styles.lastgrid}>
+            <div className={styles.lastgriditem1}>
+              <Image src="/iphone.png" height={450} width={600} />
+            </div>
+
+            <div className={styles.lastgriditem2}>
+            <div className={styles.imgcontainer}>  <Image src="/logo.png" height={100} width={100} /></div>
+              <div className={styles.lastheading}>Get The Best Wellness CRM</div>
+              <p>The Best Wellness Crm for Wellness CRM</p>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
 
 
 
